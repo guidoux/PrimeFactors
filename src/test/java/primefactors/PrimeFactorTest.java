874237b8609af39dcbd93d4a9e6d473f97703e50
guidoux <pedroguidoux@gmail.com>
2017-06-06 14:27:10 -0300

    PrimeFactor's generate method should return a list containing [2,2,3,11] when input is 132

--- src/test/java/primefactors/PrimeFactorTest.java
+++ src/test/java/primefactors/PrimeFactorTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.*;
@@ -37,5 +38,16 @@ public class PrimeFactorTest {
         expectedResult.add(3);
         expectedResult.add(5);
         assertEquals(expectedResult, primeFactor.generate(30));
+    }
+
+    @Test
+    public void generateShouldReturnAListContainingTwoTwoThreeAndElevenWhenInputIs132() {
+        ArrayList expectedResult = new ArrayList<Integer>();
+        expectedResult.add(2);
+        expectedResult.add(2);
+        expectedResult.add(3);
+        expectedResult.add(11);
+        Collections.sort(expectedResult);
+        assertEquals(expectedResult, primeFactor.generate(132));
     }
 }
