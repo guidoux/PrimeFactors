9c4dae937f0490637764d535d31d3004a646a4cd
guidoux <pedroguidoux@gmail.com>
2017-06-06 13:53:44 -0300

    PrimeFactor's generate method should return an empty list when input is one

--- src/main/java/primefactors/PrimeFactor.java
+++ src/main/java/primefactors/PrimeFactor.java
@@ -1,7 +1,13 @@
 package primefactors;
 
+import java.util.ArrayList;
+
 public class PrimeFactor {
     public static void main(String[] args) {
 
+    }
+
+    public ArrayList<Integer> generate(int number) {
+        return new ArrayList<Integer>();
     }
 }
--- src/test/java/primefactors/PrimeFactorTest.java
+++ src/test/java/primefactors/PrimeFactorTest.java
@@ -4,11 +4,17 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.*;
 
 public class PrimeFactorTest {
+    PrimeFactor primeFactor;
+
     @Before
     public void setUp() throws Exception {
+        primeFactor = new PrimeFactor();
     }
 
     @After
@@ -19,4 +25,8 @@ public class PrimeFactorTest {
     public void main() throws Exception {
     }
 
+    @Test
+    public void generateShouldReturnAnEmptyListWhenInputIsOne() {
+        assertTrue(primeFactor.generate(1).isEmpty());
+    }
 }
