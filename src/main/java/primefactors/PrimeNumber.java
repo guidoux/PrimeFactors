fe4e9e18e6b677e5f766ca43af725b066d65bf2a
guidoux <pedroguidoux@gmail.com>
2017-06-06 11:29:18 -0300

    One is not a prime number

--- src/main/java/primefactors/PrimeNumber.java
+++ src/main/java/primefactors/PrimeNumber.java
@@ -1,4 +1,14 @@
 package primefactors;
 
 public class PrimeNumber {
+    public PrimeNumber() {
+
+    }
+
+    public boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        return true;
+    }
 }
--- src/test/java/primefactors/PrimeNumberTest.java
+++ src/test/java/primefactors/PrimeNumberTest.java
@@ -2,16 +2,25 @@ package primefactors;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PrimeNumberTest {
+    private PrimeNumber prime;
+
     @Before
     public void setUp() throws Exception {
+        prime = new PrimeNumber();
     }
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void oneIsNotAprimeNumber() {
+        assertFalse(prime.isPrime(1));
     }
 
 }
